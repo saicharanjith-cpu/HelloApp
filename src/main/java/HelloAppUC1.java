aaa6a5ab85709469916a4ab58355fe15d5f7b3de
saicharanjith-cpu <saicharanjith@gmail.com>
2026-03-17 11:02:02 +0530

    Add HelloAppUC1 to display 'HelloAdd HelloAppUC1.java - UC1: Display Hello World World'

---
+++ src/main/java/HelloAppUC1.java
@@ -0,0 +1,9 @@
+// Use Case 1: Display "Hello World"
+// Student: Sai Charanjith (RA2511033010170)
+// Assignment: B2P16-AO1-Sem2-2025-29 - Hello App UC1
+
+public class HelloAppUC1 {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
